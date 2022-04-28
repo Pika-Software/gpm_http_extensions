29acdcd4d5114b84602c38d356aa736b5c561bc8
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-04-28 15:28:57 +0400

    Correction

--- lua/packages/http_extensions/main.lua
+++ lua/packages/http_extensions/main.lua
@@ -20,7 +20,7 @@ if SERVER then
     if pcall( require, "chttp" ) and (CHTTP ~= nil) then
         HTTP = CHTTP
     else
-        log( "I couldn't download CHHTP, you probably didn't download it,\n I highly recommend install CHTTP - dll module, because Garry's Mod HTTP broken by Rubat...\nhttps://github.com/timschumi/gmod-chttp/releases" ):setTag( packageName )
+        log( "I couldn't load CHHTP, you probably didn't download it,\n I highly recommend install CHTTP - dll module, because Garry's Mod HTTP broken by Rubat...\nhttps://github.com/timschumi/gmod-chttp/releases" ):setTag( packageName )
     end
 end
 
