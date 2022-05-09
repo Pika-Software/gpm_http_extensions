66a9b515357f61b76987b357318e347908413e16
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-05-09 18:34:52 +0400

    1.3.0

--- lua/packages/http_extensions/package.lua
+++ lua/packages/http_extensions/package.lua
@@ -2,7 +2,7 @@ Name = "HTTP Extensions"
 Description = "Library extending HTTP and correcting Rubat errors."
 
 Author = "PrikolMen:-b"
-Version = "1.2.4"
+Version = "1.3.0"
 License = "MIT"
 
 Dependencies = {
